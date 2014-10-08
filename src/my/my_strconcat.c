e10a73d50be06adf569b310d96cfce4e482854c6
Lazor Gator <tmiranda@stevens.edu>
2014-10-08 06:22:13 -0400

    Corrected my_strconcat

--- src/my/my_strconcat.c
+++ src/my/my_strconcat.c
@@ -7,27 +7,20 @@
 char *
 my_strconcat(char* s1, char* s2)
 {
-	int i, j;
+	int i;
 	char *ns;
 
-	/* This function assumes that the given s1 is properly allocated */
-	if (s1 != NULL && s2 != NULL)
-		ns = (char *)xmalloc(my_strlen(s1) + my_strlen(s2) + 1);
-	else if (s1 == NULL && s2 != NULL)
-		ns = (char *)xmalloc(my_strlen(s2) + 1);
-	else if (s1 != NULL && s2 == NULL)
-		ns = (char *)xmalloc(my_strlen(s1) + 1);
-	else
-		return (NULL);
-
-	i = 0;
-	if (s1 != NULL)
-		for (; s1[i] != '\0'; i++)
-			ns[i] = s1[i];
-	if (s2 != NULL)
-		for (j = 0; s2[j] != '\0'; i++, j++)
-			ns[i] = s2[j];
-	ns[i] = '\0';
+	ns = NULL;
+	if (s1 != NULL && s2 != NULL) {
+		i = 0;
+		if (s1 != NULL)
+			for (; *s1 != '\0'; s1++, i++)
+				ns[i] = *s1;
+		if (s2 != NULL)
+			for (; *s2 != '\0'; s2++, i++)
+				ns[i] = *s2;
+		ns[i] = '\0';
+	}
 
 	return (ns);
 }
