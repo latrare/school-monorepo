ed3d07680a7a942d176cc747d882430468e22427
Lazor Gator <tmiranda@stevens.edu>
2014-10-08 06:22:36 -0400

    Implemented my_vect2str()

--- src/my/my_vect2str.c
+++ src/my/my_vect2str.c
@@ -3,4 +3,20 @@
 char *
 my_vect2str(char **v)
 {
+	int i, slen, vlen;
+	char *str;
+
+	str = NULL;
+	if (v != NULL && *v != NULL) {
+		for (vlen = 0, slen = 0; v[vlen] != NULL; vlen++)
+			slen += my_strlen(v[vlen]) + 1;
+		str = (char *)xmalloc(slen);
+		for (i = 0; i < vlen; i++) {
+			str = my_strcat(str, v[i]);
+			if (i < vlen - 1)
+				str = my_strcat(str, " ");
+		}
+	}
+
+	return (str);
 }
