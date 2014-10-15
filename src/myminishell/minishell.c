043a31450fbe7a2faf3d214f87d76228b7edabd5
Lazor Gator <tmiranda@stevens.edu>
2014-10-15 00:42:15 -0400

    Completed minishell assignment

--- src/myminishell/minishell.c
+++ src/myminishell/minishell.c
@@ -1,3 +1,10 @@
+/*
+ * Trevor Miranda
+ * 
+ * Project Description
+ * -------------------
+ * Create mini wrapper shell for installed shell
+ */
 #include "my.h"
 
 int
@@ -7,9 +14,8 @@ main()
 	char buff[256];
 	char **v;
 
-	for (i = 0; i < 256; i++)
-		buff[i] = 0;
-
+	my_str("Welcome to the minishell for CS392!\n");
+	my_str("-----------------------------------\n");
 	for (;;) {
 		my_str("minish #> ");
 		if (read(STDIN_FILENO, buff, 256) < 0) {
@@ -18,29 +24,34 @@ main()
 		}
 		if ((v = my_str2vect(buff)) == NULL) {
 			my_str("Internal error when converting input to vector.\n");
-			exit(1);
+			continue;
 		}
 		if (my_strcmp(v[0], "cd") == 0) {
 			if (chdir(v[1]) < 0)
 				my_str("Failed to change working directory.\n");
-			continue;
-		}
-		if (my_strcmp(v[0], "exit") == 0) {
+			else {
+				my_str("Switched to directory: ");
+				my_str(getcwd(buff, 256));
+				my_char('\n');
+			}
+		} else if (my_strcmp(v[0], "exit") == 0) {
 			my_str("So long and thanks for all the fish!\n");
 			exit(1);
-		}
-		if ((pid = fork()) < 0) {
-			my_str("Failed to fork to execute command.\n");
-			exit(1);
-		}
-		if (pid > 0) {
-			wait();
-			free(v);
 		} else {
-			if (execvp(v[0], v) < 0)
-				my_str("Invalid command entered.\n");
-			exit(0);
+			if ((pid = fork()) < 0) {
+				my_str("Failed to fork to execute command.\n");
+				exit(1);
+			}
+			if (pid > 0)
+				wait();
+			else {
+				if (execvp(v[0], v) < 0)
+					my_str("Invalid command entered.\n");
+			}
 		}
+		my_freevect(v);
+		for (i = 0; i < 256; i++)
+			buff[i] = 0;
 	}
 
 	return (0);
