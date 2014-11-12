6fea8dc4db988d9ea16c9ebeaf4d4b18ab1a872e
Lazor Gator, The Godkiller <tmiranda@stevens.edu>
2014-11-11 22:05:00 -0500

    Re-tab, fix path, organize includes

--- include/myselect.h
+++ include/myselect.h
@@ -2,15 +2,17 @@
 #define _MYSELECT_H_
 
 #include <sys/ioctl.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <termios.h>
 #include <sys/termios.h>
-#include "../lib/my.h"
+
+#include <fcntl.h>
 #include <signal.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termcap.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "my.h"
 
 #define READTIME 1
 /* used to return from read after 1/10th of  second passed from the first byte read */
@@ -42,65 +44,66 @@
 #define VICAP "\E[?25l" // same as above but for VI
 #define ESC 27
 
-typedef struct	s_elem
+typedef struct s_elem
 {
-  char		*elem;
-  int		size;
-  int		x;
-  int		y;
-  int		mode;
-}		t_elem;
+	char *elem;
+	int  size;
+	int  x;
+	int  y;
+	int  mode;
+} t_elem;
 
-typedef struct	s_env
+typedef struct s_env
 {
-  char		*left;
-  char		*right;
-  char		*up;
-  char		*down;
-  char		*esc;
-  char		*underline;
-  char		*under_end;
-  char		*standout;
-  char		*stand_end;
-  char		*move;
-  char		*clear;
-  char		*cursoroff;
-  char		*cursoron;
-  struct winsize win;
-  struct termio	line_backup;
-  int		flag;
-  int		stdio_backup;
-  int		nbelems;
-  int		pos;
-  t_elem	*elements;
-}		t_env;
+	char           *left;
+	char           *right;
+	char           *up;
+	char           *down;
+	char           *esc;
+	char           *underline;
+	char           *under_end;
+	char           *standout;
+	char           *stand_end;
+	char           *move;
+	char           *clear;
+	char           *cursoroff;
+	char           *cursoron;
+	struct winsize win;
+	struct termio  line_backup;
+	int            flag;
+	int            stdio_backup;
+	int            nbelems;
+	int            pos;
+	t_elem         *elements;
+} t_env;
 
-t_env	gl_env;
+t_env gl_env;
 // created as a structure to limit number of globals to 1
 
-void	init_terminal();
-void	restore_terminal();
-char	*term_get_cap(char*);
-void	init_caps();
-int	my_termprint(int);
-void	term_clear();
-void	term_vi();
-void	term_ve();
-char	check_char(char *c);
-char	get_win_size();
-void	show_elems();
-void	refreshin();
-void	refreshout(int);
-void	moveup();
-void	movedown();
-void	moveleft();
-void	moveright();
-void	doselect();
-void	getout(char);
-void	setup_elems(int, char**);
-void	term_move_to_item(int);
-void	term_underline();
-void	term_standout();
-void	term_standend();
-void	term_underend();
+void init_terminal();
+void restore_terminal();
+char *term_get_cap(char*);
+void init_caps();
+int  my_termprint(int);
+void term_clear();
+void term_vi();
+void term_ve();
+char check_char(char *c);
+char get_win_size();
+void show_elems();
+void refreshin();
+void refreshout(int);
+void moveup();
+void movedown();
+void moveleft();
+void moveright();
+void doselect();
+void getout(char);
+void setup_elems(int, char**);
+void term_move_to_item(int);
+void term_underline();
+void term_standout();
+void term_standend();
+void term_underend();
+
 #endif
