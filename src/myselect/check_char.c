6e960d48181dba432b00b31782447862be4b7dc2
Lazor Gator <tmiranda@stevens.edu>
2014-11-14 07:15:47 -0500

    Fix logic error

--- src/myselect/check_char.c
+++ src/myselect/check_char.c
@@ -8,22 +8,19 @@ void
 check_char(char *c)
 {
 	if (c != NULL) {
-		if (gl_env.flag != 1) {
-			if (my_strcmp(c, KU) == 0)
-				moveup();
-			else if (my_strcmp(c, KD) == 0)
-				movedown();
-			else if (my_strcmp(c, KL) == 0)
-				moveleft();
-			else if (my_strcmp(c, KR) == 0)
-				moveright();
-			else if (my_strcmp(c, " ") == 0)
-				doselect();
-		} else {
-			if (my_strcmp(c, "\E") == 0)
-				getout((char)0);
-			else if (my_strcmp(c, "\n") == 0)
-				getout((char)1);
-		}
+		if (my_strcmp(c, KU) == 0 && gl_env.flag != 1)
+			moveup();
+		else if (my_strcmp(c, KD) == 0 && gl_env.flag != 1)
+			movedown();
+		else if (my_strcmp(c, KL) == 0 && gl_env.flag != 1)
+			moveleft();
+		else if (my_strcmp(c, KR) == 0 && gl_env.flag != 1)
+			moveright();
+		else if (my_strcmp(c, " ") == 0 && gl_env.flag != 1)
+			doselect();
+		else if (my_strcmp(c, "\E") == 0)
+			getout(0);
+		else if (my_strcmp(c, "\n") == 0)
+			getout(1);
 	}
 }
