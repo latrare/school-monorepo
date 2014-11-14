b2b84f5816df7ed1baf7d086d438d43f017f93d7
Lazor Gator, The Godkiller <tmiranda@stevens.edu>
2014-11-14 06:16:48 -0500

    More explicit error checking

--- src/myselect/check_char.c
+++ src/myselect/check_char.c
@@ -9,20 +9,20 @@ check_char(char *c)
 {
 	if (c != NULL) {
 		if (gl_env.flag != 1) {
-			if (!my_strcmp(c, KU))
+			if (my_strcmp(c, KU) == 0)
 				moveup();
-			else if (!my_strcmp(c, KD))
+			else if (my_strcmp(c, KD) == 0)
 				movedown();
-			else if (!my_strcmp(c, KL))
+			else if (my_strcmp(c, KL) == 0)
 				moveleft();
-			else if (!my_strcmp(c, KR))
+			else if (my_strcmp(c, KR) == 0)
 				moveright();
-			else if (!my_strcmp(c, " "))
+			else if (my_strcmp(c, " ") == 0)
 				doselect();
 		} else {
-			if (!my_strcmp(c, "\E"))
+			if (my_strcmp(c, "\E") == 0)
 				getout((char)0);
-			else if(!my_strcmp(c, "\n"))
+			else if (my_strcmp(c, "\n") == 0)
 				getout((char)1);
 		}
 	}
