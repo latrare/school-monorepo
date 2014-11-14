bc41944f46c5b503f73d1a923edfdbc8feca5846
Lazor Gator, The Godkiller <tmiranda@stevens.edu>
2014-11-14 06:20:34 -0500

    Fix constants used for term_get_cap

--- src/myselect/init_caps.c
+++ src/myselect/init_caps.c
@@ -16,11 +16,10 @@ init_caps()
 	}
 	tgetent(blockptr, term);
 
-	gl_env.left = term_get_cap(KL);
-	gl_env.right = term_get_cap(KR);
-	gl_env.up = term_get_cap(KU);
-	gl_env.down = term_get_cap(KD);
-	gl_env.esc = term_get_cap("\E");
+	gl_env.left = term_get_cap(LEFT);
+	gl_env.right = term_get_cap(RIGHT);
+	gl_env.up = term_get_cap(UP);
+	gl_env.down = term_get_cap(DOWN);
 	gl_env.underline = term_get_cap(UNDERLINE);
 	gl_env.under_end = term_get_cap(UNDER_END);
 	gl_env.standout = term_get_cap(STANDOUT);
