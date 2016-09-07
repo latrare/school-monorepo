7e6e1e3ded9e95809e8a02a213046e149b6b91e7
Lazor Gator, The Godkiller <trevor.mir@gmail.com>
2016-09-06 22:49:02 -0400

    Move to multiprocessing

--- ass01/main.py
+++ ass01/main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import itertools
 import hashlib
 import logging
+import multiprocessing
 import os
 import threading
 import string
@@ -20,11 +21,14 @@ SALT = '_1984'
 def bruteforce_worker(passwords, trial):
     hashes = tuple(passwords.keys())
     cracked = []
-    if hashlib.sha256(trial.encode('utf-8')).hexdigest() in hashes:
-        cracked.append(trial)
-    return cracked
+    for t in trial:
+        t = ''.join(t)
+        if hashlib.sha256(t.encode('utf-8')).hexdigest() in hashes:
+            cracked.append(t)
+    if cracked:
+        print('Cracked passwords: {}'.format(cracked))
 
-        
+
 def main():
     # Parse arguments
     parser = argparse.ArgumentParser(description='Password cracker.')
@@ -68,21 +72,45 @@ def main():
     # Thread pool lifeguard spin waiting
     start = time.time()
     print('[+] Start datetime: {}'.format(datetime.today().isoformat()))
+    with multiprocessing.Pool(processes=14) as pool:
+        for n in range(1, 7):
+            processes = []
+            print('[+] Bruteforcing passwords of length: {}'.format(n))
+            perms = itertools.permutations(list(string.printable), n)
+            while True:
+                s = list(itertools.islice(perms, 10000000))
+                if not s:
+                    break
+                proc = pool.apply_async(bruteforce_worker, (passwords, s))
+                processes.append(proc)
+
+            for p in processes:
+                p.get()
+'''
     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as pool:
+        futures = []
         for n in range(1, 7):
             print('[+] Bruteforcing passwords of length: {}'.format(n))
+            future = pool.submit(bruteforce_worker, passwords, itertools.permutations(list(string.printable), n))
+            futures.append(future)
+
+        for f in futures:
+            res = f.result()
+            if len(res):
+                passlen = len(res[-1])
+                print('[+] Passwords of length {} cracked: {}'.format(passlen, res))
 
             futures = []
             for s in itertools.permutations(list(string.printable), n):
                 futures.append(pool.submit(bruteforce_worker, passwords, ''.join(s)))
-        
+
             # Wait for results from futures
             for f in concurrent.futures.as_completed(futures):
                 res = f.result()
                 if len(res):
                     print('[+] Passwords cracked: {}.'.format(repr(res)))
-                    
+'''
 
-                    
+
 if __name__ == '__main__':
     main()
