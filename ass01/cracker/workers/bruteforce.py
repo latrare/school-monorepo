2aa75b0c657c2bbd4c3cb67b994678a622aa3973
Lazor Gator, the Godkiller <trevor.mir@gmail.com>
2016-09-14 22:35:01 -0400

    Create bruteforcer class to allow impa() and pass necessary arguments

--- ass01/cracker/workers/bruteforce.py
+++ ass01/cracker/workers/bruteforce.py
@@ -1,13 +1,26 @@
+import hashlib
+
 from ..files import write_result
 
 
-def bruteforce(trials, passwords, resfile, salt=None):
-    cracked = []
-    for trial in trials:
-        password = ''.join(password if not salt else password + salt)
-        sha256 = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        if sha256 in passwords:
-            for uid in passwords[sha256]:
-                cracked.append((password, uid, sha256))
-                write_result(resfile, password, uid, sha256)
-    return cracked
+class Bruteforcer:
+    """Class that allows passing of necessary arguments to bruteforcing function
+    while still allowing use of imap_unordered() call, which prohibits multiple
+    arguments from caller
+    """
+    def __init__(self, passwords, resfile, salt=None):
+        self.passwords = passwords
+        self.resfile = resfile
+        self.salt = salt
+
+    def bruteforce(self, trials):
+        cracked = []
+        for trial in trials:
+            # Necessary concatenation due to itertools.product() return tuples
+            trial = ''.join(trial if not self.salt else trial + self.salt)
+            sha256 = hashlib.sha256(trial.encode('utf-8')).hexdigest()
+            if sha256 in self.passwords:
+                for uid in self.passwords[sha256]:
+                    cracked.append((trial, uid, sha256))
+                    write_result(self.resfile, trial, uid, sha256)
+        return cracked
