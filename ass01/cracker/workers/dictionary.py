d14ed01bf79ed2a07a84f89acbc34b959edbd766
Lazor Gator, the Godkiller <trevor.mir@gmail.com>
2016-09-14 22:51:24 -0400

    Add dictionary entry sha256 generation function

--- ass01/cracker/workers/dictionary.py
+++ ass01/cracker/workers/dictionary.py
@@ -1,7 +1,19 @@
+import hashlib
+
 from ..files import write_result
 
 
-def dictionary_worker(trials, dictionary, resfile, salt=None):
+def dictionary_generate_sha256(dictionary):
+    'Generates dictionary of sha256 hashes for list of dictionary words'
+    realdict = dict()
+    for entry in dictionary:
+        sha256 = hashlib.sha256(entry.encode('utf-8')).hexdigest()
+        realdict[sha256] = entry
+
+    return realdict
+
+
+def dictionary(trials, dictionary, resfile, salt=None):
     cracked = []
     for trial in trials:
         sha256 = hashlib.sha256(trial.encode('utf-8'))
