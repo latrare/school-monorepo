6bc360be81809abee5949e0f3aecd272208d2c03
Lazor Gator, The Godkiller <trevor.mir@gmail.com>
2018-02-10 23:04:45 -0500

    dTree_height implemented

--- cs496/assignments/hw2.ml
+++ cs496/assignments/hw2.ml
@@ -6,7 +6,17 @@ let tLeft = Node('w', Node('x', Leaf 2, Leaf 5), Leaf 8)
 let tRight = Node('w', Node('x', Leaf 2, Leaf 5), Node('y', Leaf 7, Leaf 5))
 
 (* 3.3a *)
-let rec dTree_height (t: dTree): int = 0
+let rec dTree_height (t: dTree): int =
+  match t with
+  | Leaf n -> 0
+  | Node(c, x, y) ->
+    let x_height = dTree_height x + 1 in
+    let y_height = dTree_height y + 1 in
+    if x_height > y_height then
+      x_height
+    else
+      y_height
+
 
 (* 3.3b *)
 let rec dTree_size (t: dTree): int =
@@ -15,9 +25,6 @@ let rec dTree_size (t: dTree): int =
   | Node(c, x, y) -> 1 + (dTree_size x) + (dTree_size y)
 
 (* 3.3c *)
-let rec dTree_paths (t: dTree) =
-  match t with
-  | Leaf n ->
 
 (* 3.3d *)
 let dTree_is_perfect (t: dTree): bool = false
