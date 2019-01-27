3fb47b9784d51a8df12283010b7f2736358797ab
Trevor Miranda <trevor.miranda@pm.me>
2019-01-26 19:33:56 -0500

    Progress save on evaluator

--- ssw-555/projects/project-00/eval.py
+++ ssw-555/projects/project-00/eval.py
@@ -8,17 +8,56 @@ MONTH_MAP = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP',
              'OCT', 'NOV', 'DEC']
 
 
-Individual = namedtuple('Individual', ['id_', 'name', 'birth', 'death', 'sex'])
-Family = namedtuple('Family', ['id_',])
+class Individual(object):
+    def __init__(self, id_):
+        self.id_ = id_
+        self.deathdate = None
 
-Name = namedtuple('Name', ['first', 'last'])
-Birth = namedtuple('Birth', 'date')
-Date = namedtuple('Date', ['year', 'month', 'day'])
-Death = namedtuple('Death', 'date')
-Sex = namedtuple('Sex', 'sex')
+    def set_name(self, first, last):
+        self.name = {'first': first.capitalize(), 'last': last.capitalize()}
 
-Child = namedtuple('Child', 'family_ref')
-Spouse = namedtuple('Spouse', 'family_ref')
+    def set_birth(self, date):
+        if not date:
+            raise ValueError('Individual {} has no birthdate.'.format(self.id_))
+        self.birthdate = date
+        self._check_dates()
+
+    def set_death(self, date):
+        self.deathdate = date
+        self._check_dates()
+
+    def set_sex(self, sex):
+        self.sex = sex
+
+    def _check_dates(self):
+        if self.birthdate and self.deathdate:
+            if self.birthdate > self.deathdate:
+                    raise ValueError(
+                        'Individual {} has a death before they were born.'
+                        .format(self.id_))
+
+    def __repr__(self):
+        return ('Individual({}, {}, {})'
+                .format(self.id_, self.name['first'] + ' ' + self.name['last'],
+                        self.sex))
+
+
+class Family(object):
+    def __init__(self, id_):
+        self.id_ = id_
+
+
+IndividualId = namedtuple('IndividualId', ['id_'])
+FamilyId = namedtuple('FamilyId', ['id_'])
+
+NameExpr = namedtuple('NameExpr', ['first', 'last'])
+BirthExpr = namedtuple('BirthExpr', 'date')
+DateExpr = namedtuple('DateExpr', ['year', 'month', 'day'])
+DeathExpr = namedtuple('DeathExpr', 'date')
+SexExpr = namedtuple('SexExpr', 'sex')
+
+ChildExpr = namedtuple('ChildExpr', 'family_ref')
+SpouseExpr = namedtuple('SpouseExpr', 'family_ref')
 FamilyRef = namedtuple('FamilyRef', 'family_id')
 IndividualRef = namedtuple('IndividualRef', 'individual_id')
 
@@ -34,31 +73,33 @@ def find_arg(arg_list, type_):
 class GedcomTransformer(lark.Transformer):
     def birt_stmt(self, ts):
         (_, date) = ts
-        return Birth(date)
+        return BirthExpr(date)
 
     def deat_stmt(self, ts):
         (_, date) = ts
-        return Death(date)
+        return DeathExpr(date)
 
     def date_stmt(self, ts):
         (_, t_day, t_month, t_year) = ts
-        return Date(t_year.value, t_month.value, t_year.value)
+        return datetime.date(year=int(t_year.value),
+                             month=MONTH_MAP.index(t_month.value) + 1,
+                             day=int(t_day.value))
 
     def famc_stmt(self, ts):
         (_, t_fid) = ts
-        return Child(FamilyRef(t_fid.value))
+        return ChildExpr(FamilyRef(int(t_fid.value)))
 
     def fams_stmt(self, ts):
         (_, t_fid) = ts
-        return Spouse(FamilyRef(t_fid.value))
+        return SpouseExpr(FamilyRef(int(t_fid.value)))
 
     def name_stmt(self, ts):
         (_, t_first, t_last) = ts
-        return Name(t_first.value, t_last.value)
+        return NameExpr(t_first.value, t_last.value)
 
     def sex_stmt(self, ts):
         (_, t_sex) = ts
-        return Sex(t_sex.value)
+        return SexExpr(t_sex.value)
 
     def note_stmt(self, ts):
         # Ignore notes
@@ -68,24 +109,40 @@ class GedcomTransformer(lark.Transformer):
         (inner_list,) = ts
         indi_id = inner_list[1]
         # Arguments can be in any order, so find them by type
-        name = find_arg(inner_list, Name)
-        birth = find_arg(inner_list, Birth)
-        death = find_arg(inner_list, Death)
-        sex = find_arg(inner_list, Sex)
-        # Assemble individual
-        return Individual(indi_id.value, name, birth, death, sex)
+        return (IndividualId(int(indi_id)), (tuple(inner_list[2:]),))
+
+    def marr_stmt(self, ts):
+        pass
+
+    def husb_stmt(self, ts):
+        pass
+
+    def wife_stmt(self, ts):
+        pass
+
+    def chil_stmt(self, ts):
+        pass
+
+    def fam_stmt(self, *ts):
+        (inner_list,) = ts
+        fam_id = inner_list[1]
+        print(inner_list[1:])
+        return (FamilyId(int(fam_id)), (None,))
 
 
 def main():
-    with open('example.gedcom') as f:
+    with open('family.gedcom') as f:
         parser = lark.Lark.open('gedcom.lark')
         # Parse the given gedcom file based on the gedcom.lark grammar
         tree = parser.parse(f.read())
+        lark.tree.pydot__tree_to_png(tree, 'family-parsed.png')
         # Build the AST
         ast = GedcomTransformer().transform(tree)
         print(ast.pretty())
+        print(ast)
         # Evaluate
         # Output family tree
+        lark.tree.pydot__tree_to_png(ast, 'family-eval.png')
 
 
 if __name__ == '__main__':
